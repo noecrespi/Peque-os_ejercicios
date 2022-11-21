8003378dcc4acbe991cb57b85ac850af35d39688
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-11-21 16:10:26 +0100

    delete code coments

--- python/Practica_4_Ejercicios_para realizar_con_bucles_for/isPrimo.py
+++ python/Practica_4_Ejercicios_para realizar_con_bucles_for/isPrimo.py
@@ -13,4 +13,18 @@ def isPrimo():
     print("El numero " + str(num) + " es primo")
     return True
 
+
+
+
+# if num == 0:
+        #     nums = False
+        # else: 
+        #     for n in range(2,num):
+        #         if (num % n) == 0:
+        #             print("El numero " + str(num) + " no es primo")
+        #             # return False
+            
+        #     num  = int(input("Introduce un número: "))
+        #     print("El numero " + str(num) + " es primo")
+        #     return True
 isPrimo()
